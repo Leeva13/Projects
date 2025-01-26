36bbb39081d1d3ea62cd27ad631512bfe4dc86d0
Leeva13 <Artembrk11@gmail.com>
2025-01-26 22:24:53 +0200

    Add two new projects

---
+++ JavaScript/feature-pr-4/dist/main.js
@@ -0,0 +1,16 @@
+import { initStickyNavbar } from './modules/StickyNavbar';
+import { initDropdownHover } from './modules/DropdownHover';
+import { initModalVideo } from './modules/ModalVideo';
+import { initBackToTop } from './modules/BackToTop';
+import { initCounter } from './modules/Counter';
+import { initTestimonialCarousel } from './modules/TestimonialCarousel';
+(function ($) {
+    "use strict";
+    initStickyNavbar();
+    initDropdownHover();
+    initModalVideo();
+    initBackToTop();
+    initCounter();
+    initTestimonialCarousel();
+})(jQuery);
+//# sourceMappingURL=main.js.map
